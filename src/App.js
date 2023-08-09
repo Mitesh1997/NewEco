943b0c7f184766599f83e7989957b7d60c457642
Your Name <you@example.com>
2023-08-10 00:55:47 +0530

    added inital project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './Home';
+import New from './New';
+
+function App() {
+  return (
+    <div className='Mi'>
+   <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/New.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const New = () => {
+  return (
+    <div>
+       <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+            <h1 className="title">Jacket</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+            <h1 className="title">Sneakers</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+            <h1 className="title">Woman</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+            <h1 className="title">Mens</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+      </div>
+    </div>
+  </div>;
+    </div>
+  )
+}
+
+export default New
